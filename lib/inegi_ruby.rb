0d86a184405d9d5f6d96b6d1adeec55305c46dff
Daniel Olivares <danieloddl2@hotmail.com>
2016-09-14 18:55:39 -0500

    Add BASE_URL

--- lib/inegi_ruby.rb
+++ lib/inegi_ruby.rb
@@ -1,7 +1,10 @@
 require "inegi_ruby/version"
+require "httparty"
 
 module Inegi
   class Client
-    #code
+    include HTTParty
+    
+    BASE_URL = "http://inegifacil.com/rest/indice"
   end
 end
--- test/inegi_ruby_test.rb
+++ test/inegi_ruby_test.rb
@@ -4,4 +4,8 @@ class InegiRubyTest < TestHelper
   def setup
     @inegi = Inegi::Client.new
   end
+  
+  def test_base_url
+    assert_equal "http://inegifacil.com/rest/indice", Inegi::Client::BASE_URL
+  end
 end
